d3ba989f90a3ecc13a401ce235fb3920eb64cf85
JakeClark33 <jakemclark@hotmail.com>
2021-08-15 21:41:37 -0500

    not done

--- Develop/server.js
+++ Develop/server.js
@@ -1,21 +1,20 @@
-var express = require('express');
+const express = require('express');
 // use express for out get and app and router functions
 const app = express();
 
-const { notes } = require('./db/notes');
+const path = require('path');
+
+const { notes } = require('./public/assets/js/notes');
 // Get the CSS for the index.html
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 // connect the request to display html
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-    res.send()
-});
+
 
 // create the notes api
 app.get('/api/notes', (req, res) => {
-    res.json('Hello!');
-})
-// create the port to listen on for the response
+    res.json(notes);
+});
+// create the port to listen on for the responseDevelop\public\assets\js\notes.js
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
   });
