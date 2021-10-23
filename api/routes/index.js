62f206ec1160f3e5e982f2cddebcf1b8dda76109
JakeClark33 <jakemclark@hotmail.com>
2021-10-22 22:08:44 -0500

    heroku

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,24 @@
 // require notes.js from the db folder (to access methods refer to the notes.js)get add/delete
+require('../../db/notes');
 
- 
+
+function findById(id, notesArray) {
+    const result = notessArray.filter(note => note.id === id)[0];
+    return result;
+  }
 
 //create  get post and delete routes
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
+
+  app.post('/api/notes', (req, res) => {
+    // req.body is where our incoming content will be
+    console.log(req.body);
+    res.json(req.body);
+  });
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const { notes } = require('./db/notes');
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -17,6 +18,8 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'))
   });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+  app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
+
+  module.exports= notes;
